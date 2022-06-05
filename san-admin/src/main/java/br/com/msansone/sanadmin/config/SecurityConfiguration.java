70fa9e6356bba88cf2d7a12a72a7b6ff8858aaa2
marcio sansone <msansone@gmail.com>
2022-06-05 18:04:45 +0100

    import adjusts

--- san-admin/src/main/java/br/com/msansone/sanadmin/config/SecurityConfiguration.java
+++ san-admin/src/main/java/br/com/msansone/sanadmin/config/SecurityConfiguration.java
@@ -3,7 +3,6 @@ package br.com.msansone.sanadmin.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
